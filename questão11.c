e70ebd149ba30823e707544de9ebd66037aa051d
Alan Cristian <alan.dias@dcomp.ufs.br>
2026-02-02 19:51:46 -0300

    Update questão11.c
    Abertura do arquivo.

--- questão11.c
+++ questão11.c
@@ -1,8 +1,17 @@
-//Determine em qual edição olímpica um determinado país conquistou a sua primeira medalha.
+//todos os imports necessários.
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//definindo o tamanho máximo, em bytes de cada linha.
+#define MAX_LINE 1024
 
 int main(){
-    
-    return 0;
+    //primeiramente, o mais essencial será abrir o arquivo.
+    FILE *arquivo = fopen("results.csv","r");
+    if (!arquivo) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
 }
